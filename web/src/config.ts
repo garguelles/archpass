b781508332eebd23858d74ea8d1e78778cf3d897
garguelles <gerard.arguelles@gmail.com>
2024-11-16 14:18:21 +0800

    feat: auth provider

--- web/src/config.ts
+++ web/src/config.ts
@@ -6,3 +6,5 @@ export const NEXT_PUBLIC_URL =
 // Add your API KEY from the Coinbase Developer Portal
 export const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
 export const NEXT_PUBLIC_WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
+export const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const DEFAULT_CHAIN_ID = process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID;
