d666fdb7557cad6db0b0285fd3b6803b9e829169
garguelles <gerard.arguelles@gmail.com>
2024-11-17 00:05:52 +0800

    chore: cleanup

--- web/src/app/(dashboard)/dashboard/create-event-modal.tsx
+++ web/src/app/(dashboard)/dashboard/create-event-modal.tsx
@@ -14,13 +14,8 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Label } from '@/components/ui/label';
-import type { Address, ContractFunctionParameters } from 'viem';
-import {
-  BASE_SEPOLIA_CHAIN_ID,
-  eventFactoryABI,
-  mintABI,
-  mintContractAddress,
-} from '@/constants';
+import type { ContractFunctionParameters } from 'viem';
+import { BASE_SEPOLIA_CHAIN_ID, eventFactoryABI } from '@/constants';
 import {
   Transaction,
   TransactionButton,
@@ -43,10 +38,8 @@ type FormData = {
 
 export function CreateEventModal() {
   const [open, setOpen] = useState(false);
-  const router = useRouter();
   const {
     register,
-    handleSubmit,
     formState: { errors },
     getValues,
   } = useForm<FormData>();
