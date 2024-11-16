48d51495ed7bc04f83effb53e171055f3dfac90e
garguelles <gerard.arguelles@gmail.com>
2024-11-16 13:57:27 +0800

    feat: dashboard wallet

--- web/src/app/(dashboard)/dashboard/layout.tsx
+++ web/src/app/(dashboard)/dashboard/layout.tsx
@@ -10,6 +10,7 @@ import dynamic from 'next/dynamic';
 import '@/app/global.css';
 import '@coinbase/onchainkit/styles.css';
 import '@rainbow-me/rainbowkit/styles.css';
+import LoginButton from '@/components/LoginButton';
 
 const OnchainProviders = dynamic(
   () => import('src/components/OnchainProviders'),
@@ -29,73 +30,74 @@ export default function DashboardLayout({
   return (
     <html lang="en" className="h-full">
       <body className="h-full bg-background text-foreground">
-        <div className="flex min-h-screen">
-          {/* Sidebar */}
-          {showSidebar && (
-            <aside className="hidden md:flex flex-col w-64 bg-card text-card-foreground border-r min-h-screen">
-              <div className="p-4">
-                <div className="text-2xl font-bold mb-6">ArchPass</div>
-                <nav className="space-y-2">
-                  <Button
-                    variant="ghost"
-                    className="w-full justify-start"
-                    asChild
-                  >
-                    <Link href="/dashboard">
-                      <LayoutDashboard className="mr-2 h-4 w-4" />
-                      Dashboard
-                    </Link>
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    className="w-full justify-start"
-                    asChild
-                  >
-                    <Link href="/dashboard/slug/tickets">
-                      <Tickets className="mr-2 h-4 w-4" />
-                      Tickets
-                    </Link>
-                  </Button>
+        <OnchainProviders>
+          <div className="flex min-h-screen">
+            {/* Sidebar */}
+            {showSidebar && (
+              <aside className="hidden md:flex flex-col w-64 bg-card text-card-foreground border-r min-h-screen">
+                <div className="p-4">
+                  <div className="text-2xl font-bold mb-6">ArchPass</div>
+                  <nav className="space-y-2">
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start"
+                      asChild
+                    >
+                      <Link href="/dashboard">
+                        <LayoutDashboard className="mr-2 h-4 w-4" />
+                        Dashboard
+                      </Link>
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start"
+                      asChild
+                    >
+                      <Link href="/dashboard/slug/tickets">
+                        <Tickets className="mr-2 h-4 w-4" />
+                        Tickets
+                      </Link>
+                    </Button>
 
-                  <Button
-                    variant="ghost"
-                    className="w-full justify-start"
-                    asChild
-                  >
-                    <Link href="/dashboard/slug/attendees">
-                      <Users className="mr-2 h-4 w-4" />
-                      Attendees
-                    </Link>
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    className="w-full justify-start"
-                    asChild
-                  >
-                    <Link href="/dashboard/settings">
-                      <Settings className="mr-2 h-4 w-4" />
-                      Settings
-                    </Link>
-                  </Button>
-                </nav>
-              </div>
-            </aside>
-          )}
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start"
+                      asChild
+                    >
+                      <Link href="/dashboard/slug/attendees">
+                        <Users className="mr-2 h-4 w-4" />
+                        Attendees
+                      </Link>
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start"
+                      asChild
+                    >
+                      <Link href="/dashboard/settings">
+                        <Settings className="mr-2 h-4 w-4" />
+                        Settings
+                      </Link>
+                    </Button>
+                  </nav>
+                </div>
+              </aside>
+            )}
 
-          {/* Main content */}
-          <div className="flex-1">
-            <header className="bg-card text-card-foreground p-4 border-b">
-              <div className="text-2xl font-bold">ArchPass</div>
-            </header>
-            <main className="p-4 md:p-8">
-              <Card>
-                <CardContent className="p-6">
-                  <OnchainProviders>{children}</OnchainProviders>
-                </CardContent>
-              </Card>
-            </main>
+            {/* Main content */}
+            <div className="flex-1">
+              <header className="flex justify-end bg-card text-card-foreground p-4 border-b">
+                <LoginButton />
+              </header>
+
+              <main className="p-4 md:p-8">
+                <Card>
+                  <CardContent className="p-6">{children}</CardContent>
+                </Card>
+              </main>
+            </div>
           </div>
-        </div>
+        </OnchainProviders>
       </body>
     </html>
   );
